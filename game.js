553422ba530e2582b1a48dec0c7c1ce754287fcd
Tim Sharp <timothyaaron.sharp@gmail.com>
2017-10-27 16:40:14 -0600

    Game works + comments

--- game.js
+++ game.js
@@ -1,128 +1,70 @@
 //IF STATEMENT FOR AFTER CHOOSING WHICH TYPE YOU AND THE COMP WILL USE TO DECIDE THE MODIFIER IE. WATER VS FIRE 1.5X?
 var damage = 1
 var allMods = 1
-// var toolBelt = enemy.equipment
-function Items(name, modifier, description) {
+var enemy = new Target('Whatever Name You Chose', 100)
+
+function Items(name, modifier, description) { //<--Constructor to build items.
     this.name = name
     this.modifier = modifier
     this.description = description
 }
-
-function Target(name, health) {
+function Target(name, health) {              //<--Constructor to build enemies.
     this.name = name
     this.health = health
     this.hits = 0
     this.equipment = []
 }
-
-var inventory = {
-    shield: new Items("Shield", -0.3, "This is an awesome shield!"),
-    maul: new Items("Maul", 0.5, "IT'S HAMMERTIME!"),
-    armorPot: new Items("Armor Potion", -0.4, "It's medicinal")
+var inventory = {                           //Possible item choices.
+    shield: new Items("Shield", -0.3, "This is an awesome shield!"),//Item objects
+    maul: new Items("Maul", 0.5, "IT'S HAMMERTIME!"),               //made with
+    armorPot: new Items("Armor Potion", -0.4, "It's medicinal")     //a constructor.
 }
-//console.log(inventory)
-var enemy = new Target('Whatever Name You Chose', 100)
-// console.log(enemy)
-function healthCheck() {
+
+function healthCheck() {                    //function to stop health going below 0.
     if (enemy.health < 0) {
         enemy.health = 0
     }
 }
-function slap() {
+function slap() {                           //function for the slap attack.
     addMods()
     enemy.health -= damage * allMods
     enemy.hits++
     healthCheck()
     update()
 }
-console.log(damage * allMods)
-function punch() {
+function punch() {                          //function for punch attack.
     addMods()
     enemy.health -= (damage * 5) * allMods
     enemy.hits++
     healthCheck()
     update()
 }
-console.log((damage * 5) * allMods)
-function kick() {
+function kick() {                           //function for kick attack.
     addMods()
     enemy.health -= (damage * 10) * allMods
     enemy.hits++
     healthCheck()
     update()
 }
-console.log((damage * 10) * allMods)
-function update() {
+function update() {                         //function that updates info on page.
     document.getElementById('name').innerText = enemy.name
     document.getElementById('health').innerText = enemy.health.toFixed(2)
     document.getElementById('hits').innerText = enemy.hits
 }
-function giveShield(inventory) {
+function giveShield(inventory) {            //function to give shield to enemy.
     enemy.equipment.push(inventory.shield)
 }
-function giveMaul(inventory) {
+function giveMaul(inventory) {              //function to give to maul to enemy.
     enemy.equipment.push(inventory.maul)
 }
-function giveArmorPot(inventory) {
+function giveArmorPot(inventory) {          //function to give armor pot to enemy.
     enemy.equipment.push(inventory.armorPot)
 }
-function addMods() {
+function addMods() {                        //function to apply item modifiers.
     var out = 1
     for (var i = 0; i < enemy.equipment.length; i++) {
-        out += enemy.equipment[i].modifier
-        // console.log(out)
+        out += enemy.equipment[i].modifier 
     }
     allMods = out.toFixed(2)
-    // console.log(allMods)
 }
-// function addItems(giveShield, giveMaul, giveArmorPot){
-//     document.getElementById("giveShield").onclick = giveShield(inventory)
-//     document.getElementById("giveMaul").onclick = giveMaul(inventory)
-//     document.getElementById("giveArmorPot").onclick = giveArmorPot(inventory)
-// }
-console.log(enemy)
 update()
-// giveShield(inventory)
-// giveMaul(inventory)
-// giveArmorPot(inventory)
-
-// x = 1
-// console.log(x.toFixed(2))
-
-
-/*INCASE REFACTORING WRECKS MY CODE
-var health = 100
-var name = 'Whatever Name You Chose'
-var hits = 0
-function healthCheck() {
-    if (health < 0) {
-        health = 0
-    }
-}
-function slap() {
-    health -= 1
-    hits++
-    healthCheck()
-    update()
-}
-//slap()
-//punch()
-//kick()
-function punch() {
-    health -= 5
-    hits++
-    healthCheck()
-    update()
-}
-function kick() {
-    health -= 10
-    hits++
-    healthCheck()
-    update()
-}
-function update() {
-    document.getElementById('name').innerText = name
-    document.getElementById('health').innerText = health
-    document.getElementById('hits').innerText = hits
-}
-update() */
